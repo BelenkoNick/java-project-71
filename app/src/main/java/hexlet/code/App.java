47e8239606337f3ada7fef285c5152d9f24c1ef3
BelenkoNick <belenko.nr@gmail.com>
2024-08-24 13:45:06 +0300

    Добавлен Makefile и picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,34 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
+        description = "Compares two files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    private static final int SUCCESS_EXIT_CODE = 0;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    boolean helpRequested;
+
+    @Option(names = { "-V", "--version" }, versionHelp = true, description = "Print version information and exit.")
+    boolean versionRequested;
+
+    @Override
+    public Integer call() {
+        return SUCCESS_EXIT_CODE;
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
